a1e653439b8bae718c102a5db9e829c3053f3e9b
mauroxcf <maurox03@gmail.com>
2022-03-17 23:40:38 -0500

    adding other half of task

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- Write a SQL script that creates a stored procedure
+-- ComputeAverageScoreForUser that computes and store
+DELIMITER //
+
+DROP PROCEDURE if EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
+UPDATE users
+SET average_score = (
+    SELECT SUM(score) / COUNT(*)
+    FROM corrections AS c
+    WHERE c.user_id = user_id
+  )
+WHERE id = user_id;
+END;
+DELIMITER; //
